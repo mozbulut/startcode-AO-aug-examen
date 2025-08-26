2d073a77452b9fb54aa4f7cc825352230edb2b37
mustafa ozbulut <mstfzblt@gmail.com>
2025-08-26 17:30:15 +0200

    add test

--- back-end/test/service/classroom.test.ts
+++ back-end/test/service/classroom.test.ts
@@ -2,21 +2,36 @@ import classroomDb from "../../repository/classroom.db";
 import classroomService from "../../service/classroom.service";
 
 let mockClassroomDbCreateClassroom: jest.Mock;
-let mockClassroomDbfindClassroomByName: jest.Mock;
+let mockClassroomDbFindClassroomByName: jest.Mock;
 
 beforeEach(() => {
     mockClassroomDbCreateClassroom = jest.fn();
-    mockClassroomDbfindClassroomByName = jest.fn();
+    mockClassroomDbFindClassroomByName = jest.fn();
 })
 
 test('given a unique classroom name, when a classroom is created, then the classroom is created successfully', async () => {
 
-    classroomDb.findClassroomByName = mockClassroomDbfindClassroomByName.mockResolvedValue(null);
+    classroomDb.findClassroomByName = mockClassroomDbFindClassroomByName.mockResolvedValue(null);
     classroomDb.createClassroom = mockClassroomDbCreateClassroom.mockResolvedValue({ id: 1, name: 'Classroom 1' });
 
     await classroomService.createClassroom('Classroom 1');
 
-    expect(mockClassroomDbfindClassroomByName).toHaveBeenCalledTimes(1);
+    expect(mockClassroomDbFindClassroomByName).toHaveBeenCalledTimes(1);
+    expect(mockClassroomDbFindClassroomByName).toHaveBeenCalledWith('Classroom 1');
     expect(mockClassroomDbCreateClassroom).toHaveBeenCalledTimes(1);
 
 })
+
+test('given a name for an existing classroom, when creating a classroom, then an error is thrown', async () => {
+    const existingClassroom = { id: 1, name: 'Classroom 1' };
+
+    classroomDb.findClassroomByName = mockClassroomDbFindClassroomByName.mockResolvedValue(existingClassroom);
+    classroomDb.createClassroom = mockClassroomDbCreateClassroom;
+
+    await expect(classroomService.createClassroom('Classroom 1'))
+        .rejects
+        .toThrow(`Classroom with name Classroom 1 is already exits`);
+
+    expect(mockClassroomDbFindClassroomByName).toHaveBeenCalledWith('Classroom 1');
+    expect(mockClassroomDbCreateClassroom).not.toHaveBeenCalled();
+});
